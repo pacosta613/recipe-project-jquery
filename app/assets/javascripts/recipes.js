f6712eb67f1d81a07dd1e45ad72210b5e5222c9f
pacosta613 <pedro.acosta613@gmail.com>
2016-07-25 16:30:01 -0400

    Ingredients and Comments are rendering perfectly

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -1,36 +1,38 @@
 $(document).ready(function(){
   loadIngredients();
-  //loadComments();
+  loadComments();
   newIngredients();
-  //newComments();
+  newComments();
 });
 
 function loadIngredients(){
   $('#recipe_ingredients').html('<h3>Ingredients:</h3>')
+
   $.ajax({
     method: 'GET',
     dataType: 'json',
-    url: this.href
+    url: this.action
   }).done(function(response){
     grabIngredients(response);
   });
 };
 
 function grabIngredients(data){ 
-  var ingredients = [];
+  var ingredients = data["ingredients"];
   var names = [];
   var nameId = [];
   var orderIngredients = "<ol>";
   
-  for (var i = 0; i < data.length; i++) {
-    names.push(data[i]["name"]);
-    nameId.push(data[i]["id"]);
+  for (var i = 0; i < ingredients.length; i++) {
+    names.push(ingredients[i]["name"]);
+    nameId.push(ingredients[i]["id"]);
   }
   for (var i = 0; i < nameId.length; i++) {
     orderIngredients += "<li>" + names[i] + "</li>";
+    
   }
   orderIngredients += "</ol>";
-  return orderIngredients;
+ $(".ingredients").html(orderIngredients);
 }
 
 function newIngredients(){
@@ -57,25 +59,26 @@ function loadComments(){
     method: 'GET',
     dataType: 'json'
   }).done(function(response){
-    grabComments(response.comments);
+    grabComments(response);
   });
 }
 
 function grabComments(array) {
-  var comments = [];
+  var comments = array["comments"];
   var commentId = [];
-  var user = [];
+  var comment = [];
+  var users = [];
   var orderComments = "<ol>";
   
-  for (var i = 0; i < array.length; i++) {
-    
-    commentsId.push(array[i]["id"]);
-    comments.push(array[i]["content"]);
-    user.push(array[i].user["email"]);
+  for (var i = 0; i < comments.length; i++) {
+    commentId.push(comments[i]["id"]);
+    comment.push(comments[i]["content"]);
+    users.push(comments[i].user["email"]);
   }
   for (var i = 0; i < commentId.length; i++){
-    orderComments += "<li>" + user[i] + " - " + comments[i] + "</li>";
+    orderComments += "<li>" + users[i] + " - " + comment[i] + "</li>";
   }
+
   orderComments += "</ol>";
   $(".comments").html(orderComments);
 };
@@ -90,8 +93,8 @@ function newComments(){
       data: $(this).serialize(),
       dataType: 'json'
     }).done(function(response){
-      $(".comments ol").append("<li>" + response["content"] + "</li>");
-      //$("#comment_content").val("");
+      $(".comments ol").append("<li>" + response.user["email"] + " - " + response["content"] + "</li>");
+      $("#comment_content").val("");
     });
   });
 }
--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :recipe_id, :content
+  attributes :id, :user_id, :recipe_id, :content, :user
   has_one :user
   has_one :recipe
 end
